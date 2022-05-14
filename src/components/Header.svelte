a801a59097c1fef8d5d785863f237468821c3320
helloljho <helloljho@gmail.com>
2022-05-14 14:06:42 +0900

    fix : logo component

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { link } from "svelte-spa-router";
+  import { link, push } from "svelte-spa-router";
   import active from "svelte-spa-router/active";
   import Logo from "~/components/Logo.svelte";
 
@@ -7,17 +7,16 @@
     {
       href: "/",
       name: "Search",
-      path: "/",
     },
     {
-      href: "/movie/tt5420988",
+      href: "/movie/tt4520988",
       name: "Movie",
       path: "/movie/*",
     },
     {
       href: "/about",
       name: "About",
-      path: "/about",
+      path: /^\/about/, // 정규표현식 사용!
     },
   ];
 </script>
@@ -28,15 +27,70 @@
     <ul>
       {#each menus as { href, name, path } (name)}
         <li>
-          <a use:link use:active={path} {href}>{name}</a>
+          <a use:link use:active={path} {href}>
+            {name}
+          </a>
         </li>
       {/each}
     </ul>
   </nav>
+  <div class="user">
+    <img src="/assets/svelte.png" alt="User" />
+  </div>
 </header>
 
 <style lang="scss">
+  header {
+    padding: 20px 40px;
+    background-color: $color--black-90;
+    position: sticky;
+    top: 0;
+    z-index: 9;
+    display: flex;
+    align-items: flex-end;
+
+    nav {
+      margin-left: 40px;
+      ul {
+        display: flex;
+        li {
+          margin-left: 10px;
+          &:first-child {
+            margin-left: 0;
+          }
+          a {
+            font-size: 14px;
+            font-weight: 700;
+            color: rgba(#fff, 0.5);
+            text-decoration: none;
+          }
+        }
+      }
+    }
+    .user {
+      width: 40px;
+      height: 40px;
+      padding: 6px;
+      box-sizing: border-box;
+      border-radius: 50%;
+      background-color: $color--area;
+      cursor: pointer;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      right: 40px;
+      margin: auto;
+      transition: 0.4s;
+
+      &:hover {
+        background-color: lighten($color--area, 20%);
+      }
+      img {
+        width: 100%;
+      }
+    }
+  }
   header :global(a.active) {
-    color: red;
+    color: $color--primary !important;
   }
 </style>
