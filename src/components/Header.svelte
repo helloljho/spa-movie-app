fb620cbf3ccc82ac7bb8cae0432c0047ded7c849
helloljho <helloljho@gmail.com>
2022-05-14 13:09:12 +0900

    fix : add logo components

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,21 +1,16 @@
 <script>
   import { link } from "svelte-spa-router";
   import active from "svelte-spa-router/active";
-
-  let title = "Header";
+  import Logo from "~/components/Logo.svelte";
 </script>
 
 <header>
+  <Logo />
   <a use:link use:active="{'/'}" href="/">Home</a>
   <a use:link use:active="{'/about'}" href="/about">About</a>
-  <h1>{title}</h1>
 </header>
 
 <style lang="scss">
-  h1 {
-    color: orange;
-  }
-
   header :global(a.active) {
     color: red;
   }
