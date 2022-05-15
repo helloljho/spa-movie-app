6d01b6aed7fc74945e96f570e1e0ddee42673eb6
helloljho <helloljho@gmail.com>
2022-05-15 13:19:43 +0900

    fix : movie card

--- src/components/MovieList.svelte
+++ src/components/MovieList.svelte
@@ -1,11 +1,27 @@
 <script>
   import { moives } from "~/store/movie";
+  import MovieCard from "~/components/MovieCard.svelte";
 </script>
 
 <div class="movie-list">
-  <div class="movie">
+  <div class="movies">
     {#each $moives as movie (movie.imdbID)}
-      {movie.Title}
+      <MovieCard {movie} />
     {/each}
   </div>
 </div>
+
+<style lang="scss">
+  .movie-list {
+    margin-top: 30px;
+    padding: 10px;
+    background-color: $color--area;
+    border-radius: 4px;
+    text-align: center;
+    .movies {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+    }
+  }
+</style>
--- src/store/movie.js
+++ src/store/movie.js
@@ -26,5 +26,4 @@ export async function searchMovies(payload) {
       moives.update(($movies) => unionBy($movies, Search, "imdbID"));
     }
   }
-  console.log(get(moives));
 }
